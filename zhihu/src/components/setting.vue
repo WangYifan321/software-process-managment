9d2b1d106b8fc408c81e42de554bc9a112e47e0b
wangyifan <wangyifan52199@163.com>
2021-12-30 10:31:51 +0800

    add selected.vue setting.vue

---
+++ zhihu/src/components/setting.vue
@@ -0,0 +1,37 @@
+<template>
+<div style="margin: 0 auto">
+  <el-link :underline="false" icon="el-icon-close" id="close" href="/"></el-link>
+
+  <h3 style="float: left;
+    margin-left: 15px;margin-top: 54px">无痕浏览</h3>
+  <el-switch
+    v-model="value"
+    active-color="#13ce66"
+    inactive-color="#ff4949"
+  style=" margin-top: 50px;
+    float: right;
+    margin-right: 10px;">
+  </el-switch>
+</div>
+</template>
+
+<script>
+export default {
+  name: "setting",
+  data(){
+    return{
+      value:true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#close{
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: -77px;
+  font-size: 20px;
+}
+</style>
